540b81cd5038e24fde21bd42676f3b0100a1b102
zhangfan <1647508697@qq.com>
2020-10-05 17:43:25 +0800

    10.5

---
+++ MianXiangDuiXiang/duotai_code3/main.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+class Animal{
+public:
+
+    Animal(){
+        cout<<"Animal构造函数"<<endl;
+    }
+
+//    ~Animal(){
+//        cout<<"Animal析构函数"<<endl;
+//    }
+    //当子类中含有指针时，不会调用子类的析构函数，所以要把父类的析构函数变为虚函数或者纯虚函数
+
+    //虚析构函数,调用虚函数的同时也调用子类的析构函数
+//    virtual ~Animal(){
+//        cout<<"Animal虚析构函数"<<endl;
+//    }
+
+    //纯虚析构函数
+    virtual ~Animal()=0;
+
+    //纯虚函数
+    virtual void speak()=0;
+};
+//类内声明，类外实现
+Animal::~Animal(){
+    cout<<"Animal纯虚析构函数"<<endl;
+}
+
+class Cat:public Animal{
+public:
+    Cat(string name){
+        cout<<"cat构造函数"<<endl;
+        this->name=new string(name);
+    }
+
+    ~Cat(){
+        cout<<"Cat析构函数"<<endl;
+        delete name;
+        name=NULL;
+    }
+
+    void speak(){
+        cout<<*name<<"叫"<<endl;
+    }
+    string* name;
+};
+
+void test(){
+    Animal* animal=new Cat("tom");
+    animal->speak();
+    delete animal;
+}
+int main()
+{
+    test();
+    return 0;
+}
---
+++ MianXiangDuiXiang/duotai_code4/main.cpp
@@ -0,0 +1,86 @@
+﻿/*
+ * 多态案例 p142
+ * 电脑接口
+ * Cpu* cpu=new IntelCpu;
+ */
+
+#include <iostream>
+
+using namespace std;
+class Cpu{
+public:
+    virtual void cpuwork()=0;
+};
+
+class Memory{
+public:
+    virtual void memorywork()=0;
+};
+
+class VideoCard{
+public:
+    virtual void videocardwork()=0;
+};
+
+class Computer{
+public:
+    Computer(Cpu* cpu,Memory* memory,VideoCard* videocard){
+        this->cpu=cpu;
+        this->memory=memory;
+        this->videocard=videocard;
+        cout<<"computer构造函数"<<endl;
+    }
+
+    ~Computer(){
+        delete cpu;
+        delete memory;
+        delete videocard;
+        cout<<"computer析构函数"<<endl;
+    }
+
+    void dowork(){
+        cpu->cpuwork();
+        memory->memorywork();
+        videocard->videocardwork();
+    }
+private:
+    Cpu* cpu;
+    Memory* memory;
+    VideoCard* videocard;
+};
+
+class IntelCpu:public Cpu{
+public:
+    void cpuwork(){
+        cout<<"intel cpu work"<<endl;
+    }
+};
+
+class IntelMemory:public Memory{
+public:
+    void memorywork(){
+        cout<<"intel memory work"<<endl;
+    }
+};
+
+class IntelVideoCard:public VideoCard{
+public:
+    void videocardwork(){
+        cout<<"intel videocard work"<<endl;
+    }
+};
+
+void test(){
+    Cpu* cpu=new IntelCpu;
+    Memory* memory=new IntelMemory;
+    VideoCard* videocard=new IntelVideoCard;
+
+    Computer* computer=new Computer(cpu,memory,videocard);
+    computer->dowork();
+    delete computer;
+}
+int main()
+{
+    test();
+    return 0;
+}
