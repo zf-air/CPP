62b61c3eb80315157ffabde347ff10877d60b277
zhangfan <1647508697@qq.com>
2021-07-29 20:51:50 +0800

    7.29

---
+++ KaoYan/2.XianXingBiao/ShuangLianBiao/cmake-build-debug/CMakeFiles/ShuangLianBiao.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShuangLianBiao.dir/main.cpp.obj"
+  "ShuangLianBiao.exe"
+  "ShuangLianBiao.exe.manifest"
+  "ShuangLianBiao.pdb"
+  "libShuangLianBiao.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ShuangLianBiao.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
